f796a8ea4e5aabbbf6fd4f57d4cb69bbb7a02cfc
Parnika <parnikapola@gmail.com>
2026-05-14 17:12:08 +0530

    eddited some files.

--- config.py
+++ config.py
@@ -2,5 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'zomato_clone_super_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    db_url = os.environ.get('DATABASE_URL')
+
+    if db_url and db_url.startswith("postgresql://"):
+        db_url = db_url.replace("postgresql://", "postgresql+psycopg2://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url or 'sqlite:///food_delivery.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
