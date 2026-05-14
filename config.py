bb1e0ab9c4c8a97fbcb3ae3b479ca62d84f02db8
Parnika <parnikapola@gmail.com>
2026-05-14 16:30:41 +0530

    eddited some files.

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'zomato_clone_super_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///food_delivery.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://food_delivery_reuc_user:7nPpC0qzChgi7ohG9BtlgwJD0Go2fxUC@dpg-d82qfn0g4nts73b8sveg-a.oregon-postgres.render.com/food_delivery_reuc')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ init_db.py
@@ -0,0 +1,12 @@
+from app import create_app
+from extensions import db
+import models   # important: loads all models
+
+print("Starting database initialization...")
+
+app = create_app()
+
+with app.app_context():
+    print("Inside app context...")
+    db.create_all()
+    print("Database created successfully!")
